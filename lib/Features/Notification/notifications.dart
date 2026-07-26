2226135500bcd68259a5944cc501e64a7a834d34
BuildwithNuel <devhurl7@gmail.com>
2026-07-26 23:32:33 +0100

    Remove unused import for googleapis_auth in notifications.dart

--- lib/Features/Notification/notifications.dart
+++ lib/Features/Notification/notifications.dart
@@ -4,7 +4,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:googleapis_auth/googleapis_auth.dart';
+
 
 class Notifications extends StatelessWidget {
   Future<void> sendTestNotification() async {
